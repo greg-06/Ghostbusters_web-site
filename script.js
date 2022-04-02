b8c07ff085f6fffc2d89460edbf56dabbe9c4239
Grégory Sibemilia <Gregory06@live.fr>
2022-04-03 00:22:32 +0200

    modifs

---
+++ script.js
@@ -0,0 +1,10 @@
+// var contenu = document.querySelector(".dropdown-1 .contenu");
+
+// contenu.onmouseover = function() {
+//     contenu.style.borderRadius = "18px";
+// };
+
+// contenu.onmouseout = function() {
+//     contenu.style.borderRadius = "18px";
+// };
+
